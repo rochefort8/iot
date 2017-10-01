5b4ad9286134f9b9ee1b2139e11b4274de2cfd19
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2017-10-01 12:06:30 +0900

    Add new tts script and data for hyakuninishu

--- aws/polly/polly.sh
+++ aws/polly/polly.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 count=0
+bn=$2
 
 for f in $(cat $1);
 do
-    basename=$(printf "%03d" $count)
+    idx=$(printf "%03d" $count)
+    basename=$idx"_"$bn
+#   aws polly synthesize-speech --output-format ogg_vorbis --voice-id Mizuki --text $f voice/$basename.ogg
    aws polly synthesize-speech --output-format mp3 --voice-id Mizuki --text $f voice/$basename.mp3
 
     echo $basename
