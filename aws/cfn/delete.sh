b63f4a6700608c9f236e8470e299cd43fac8f4e2
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2020-04-11 21:53:37 +0900

    Added AWS CloudFormation

---
+++ aws/cfn/delete.sh
@@ -0,0 +1,1 @@
+D=/Users/rochefort10/Ext/git/iot/aws/cfn;aws cloudformation delete-stack --stack-name cfn-ogi
---
+++ aws/cfn/validate.sh
@@ -0,0 +1,1 @@
+D=/Users/rochefort10/Ext/git/iot/aws/cfn;aws cloudformation validate-template --template-body file:////Users/rochefort10/Ext/git/iot/aws/cfn/0.yaml
